04694a1aba0fbcaccb7cc6173a6a83cec680b082
WendySanarwanto <wendy.sanarwanto@gmail.com>
2016-10-26 07:07:15 +0800

    Finished Section 1, video #19

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { term: '' };
+        //this.onInputChange.bind(this); // Important ! This will prevent `undefined` this, in line 20
+    }
+
     render() {
         return (
-           <input type="text"/>
+            <div>
+                <input type="text" 
+                    value={this.state.term}
+                    onChange={this.onInputChange.bind(this)} />
+            </div>           
         );
+    }
+
+    onInputChange(event){
+        let state = this.state;
+        state.term = event.target.value;        
+        console.log(`[DEBUG-SearchBar] - onInputChange is called. event.target.value=${state.term}`);
+        this.setState(state);
     }
 }
 
