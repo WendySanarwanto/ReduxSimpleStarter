6b8f95db38037c44442a6e544528feafc18c0317
WendySanarwanto <wendy.sanarwanto@gmail.com>
2016-10-25 21:54:06 +0800

    Finished Section 1, video #15

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar'
+
+const API_KEY = 'AIzaSyBYnLPP6W4XLUBFQBoPBpGtMdyUHDtwveM';
+
+// Create a new component. This component should produce some HTML
+class App extends Component {
+    render(){
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
