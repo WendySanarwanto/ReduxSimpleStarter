7007cc9ecd2ee289e85f320f32c62099edc151e1
WendySanarwanto <wendy.sanarwanto@gmail.com>
2016-10-27 04:04:24 +0800

    Finished Section 2 , video 28

---
+++ src/components/video_detail.jsx
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class VideoDetail extends Component {
+
+    render () {
+        const video = this.props.video;
+        const videoId = video.id.videoId;
+        const videoUrl = `https://www.youtube.com/embed/${videoId}`;
+
+        return (
+            <div className="video-detail col-md-8">
+                
+                <div className="embed-responsive embed-responsive-16by9">
+                    <iframe src={videoUrl} className="embed-responsive-item"></iframe>
+                </div>
+
+                <div className="details">
+                    <div>{video.snippet.title}</div>
+                    <div>{video.snippet.description}</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default VideoDetail;
