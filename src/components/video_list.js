82715f6f01dda417255270bf53dfd213db4d0d9e
WendySanarwanto <wendy.sanarwanto@gmail.com>
2018-04-12 13:24:12 +0800

    Added video detail component

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import VideoListItem from './video_list_item';
+
 const VideoList = ({videos}) => {
     const videoItems = videos.map(video => {
         return <VideoListItem key={video.etag} video={video} />
