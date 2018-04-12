f65bfce7cdc1234891c51f857de62030360a7f92
WendySanarwanto <wendy.sanarwanto@gmail.com>
2018-04-12 13:42:55 +0800

    Added onVideoSelect callback

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import VideoListItem from './video_list_item';
 
-const VideoList = ({videos}) => {
-    const videoItems = videos.map(video => {
-        return <VideoListItem key={video.etag} video={video} />
-    });
+const VideoList = ({onVideoSelect, videos}) => {
+  const videoItems = videos.map(video => {
+    return ( 
+      <VideoListItem 
+        onVideoSelect={onVideoSelect}            
+        key={video.etag} 
+        video={video} />
+    )
+  });
 
-    return (
-        <ul className="col-md-4 list-group">
-            {videoItems}
-        </ul>
-    );
+  return (
+    <ul className="col-md-4 list-group">
+        {videoItems}
+    </ul>
+  );
 };
 
 export default VideoList;
